48716193b40a35577ee192188e27a94b863741f9
maverickzzz <maverick.9988@gmail.com>
2018-06-30 17:22:30 +0700

    add favicon

--- core/config.php
+++ core/config.php
@@ -5,4 +5,11 @@ $config['dbhost'] = 'localhost';
 $config['dbname'] = 'myfoodtracker';
 $config['dbuser'] = 'root';
 $config['dbpwd'] = '';
+
+// global $config;
+// $config = array();
+// $config['dbhost'] = 'localhost';
+// $config['dbname'] = 'maverick_myfoodtracker';
+// $config['dbuser'] = 'maverick_myfood';
+// $config['dbpwd'] = 'myfoodtracker@123';
 ?>
